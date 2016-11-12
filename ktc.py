6ff37040aa974d602cab979ee427ed463ff06964
Brian Yang <brian@brianyang.me>
2016-11-12 10:13:26 -0500

    Add Amazon product lookup

--- ktc.py
+++ ktc.py
@@ -1,7 +1,7 @@
 import logging
 
 # flask framework
-from flask import Flask, render_template, Response, request
+from flask import Flask, render_template, Response, request, jsonify
 app = Flask(__name__)
 
 # amazon advertising api
@@ -9,12 +9,27 @@ from amazon.api import AmazonAPI
 import amazonkeys
 
 # constants from amazonkeys.py
-amazon = AmazonAPI(AMAZON_ACCESS_KEY, AMAZON_SECRET_KEY, AMAZON_ASSOC_TAG)
+amazon = AmazonAPI(
+    amazonkeys.AMAZON_ACCESS_KEY,
+    amazonkeys.AMAZON_SECRET_KEY,
+    amazonkeys.AMAZON_ASSOC_TAG
+)
 
 # homepage
 @app.route("/")
 def index():
     return render_template("index.html")
+
+## Look up Amazon product info by ASIN
+@app.route('/amazon/<prod_id>')
+def amazon_search(prod_id) :
+    product = amazon.lookup(ItemId=prod_id)
+    result = {
+        'name': product.title,
+        'price': product.price_and_currency[0],
+        'currency': product.price_and_currency[1]
+    }
+    return jsonify(**result)
 
 @app.errorhandler(500)
 def server_error(e):
@@ -22,4 +37,4 @@ def server_error(e):
     return 'An internal error occurred.', 500
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
